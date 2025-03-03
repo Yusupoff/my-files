9d31c5a167d1d1d0c2e40da4fe0a8500536e779a
Yusupoff <baitmail@ya.ru>
2025-03-03 16:45:04 +0300

    Update update_youtubeUnblock.sh

--- update_youtubeUnblock.sh
+++ update_youtubeUnblock.sh
@@ -1,2 +1,22 @@
 #!/bin/sh
 echo "Update youtubeUnblock"
+ARCH=$(opkg info youtubeUnblock | grep 'Architecture:' | awk '{print $2}')
+
+ENDPOINT="myhostkeenetic.zapto.org"
+PORT=5000
+REQUEST=$(cat <<EOF
+GET /send HTTP/1.1
+Host: $ENDPOINT
+Accept: application/json
+
+EOF
+)
+# Отправка запроса и получение ответа
+RESPONSE=$(echo -e "$REQUEST" | nc "$ENDPOINT" "$PORT") > /dev/null 2>&1
+
+# Извлечение JSON из ответа
+JSON=$(echo "$RESPONSE" | awk 'BEGIN {RS="\r\n\r\n"} NR==2')
+VERSION=$(echo "$JSON" | jq -r '.version')
+
+wget https://github.com/Waujito/youtubeUnblock/releases/download/v1.0.0/youtubeUnblock-$VERSION-$ARCH-openwrt-23.05.ipk -O /tmp/youtubeUnblock-$VERSION-$ARCH-openwrt-23.05.ipk
+wget https://github.com/Waujito/youtubeUnblock/releases/download/v1.0.0/luci-app-youtubeUnblock-$VERSION.ipk -O /tmp/luci-app-youtubeUnblock-$VERSION.ipk
