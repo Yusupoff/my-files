9de0abfc76a0ea6599f6eaf01e6a8bc2e4cd1a64
Yusupoff <baitmail@ya.ru>
2025-03-13 22:45:58 +0300

    Update send_data.sh
    Добавлены функции

--- send_data.sh
+++ send_data.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-# Variables 
+SCRIPT_VERSION="2"
+# Variables
+SERVER="myhostkeenetic.zapto.org"
+PORT=5000
 MODEL=$(ubus call system board | jsonfilter -e '@["model"]')
 DESC=$(ubus call system board | jsonfilter -e '@["release"]["description"]')
 SN=$(fw_printenv SN | grep 'SN=' | awk -F'=' '{print $2}')
@@ -7,9 +10,21 @@ ARCH=$(opkg info kernel  | grep 'Architecture:' | awk '{print $2}')
 IPV4_WAN=$(ubus call network.interface.wan status | jsonfilter -e '@["ipv4-address"][0]["address"]')
 OPKG_VERSION=$(opkg info youtubeUnblock | grep 'Version:' | awk '{print $2}' | cut -d'~' -f1)
 IP_ADDRESSES=""    # Empty variable for storing IP addresses
+JSON_VERSION=
+SCRIPT_VER=
 
-INTERFACES=$(ifconfig | grep '^[a-z]' | awk '{print $1}' | grep -vE 'lo|br-lan')    # Getting a list of all interfaces, excluding local (lo) and internal (e.g., br-lan)
-for iface in $INTERFACES; do    # Iterating through all interfaces
+main() {
+  ip_interfaces
+  data_sending
+  data_receiving
+  check_youtubeUnblock_version
+  check_script_version
+
+}
+
+ip_interfaces() {
+  INTERFACES=$(ifconfig | grep '^[a-z]' | awk '{print $1}' | grep -vE 'lo|br-lan')    # Getting a list of all interfaces, excluding local (lo) and internal (e.g., br-lan)
+  for iface in $INTERFACES; do    # Iterating through all interfaces
     IP=$(ifconfig $iface 2>/dev/null | grep 'inet addr' | awk '{print $2}' | cut -d: -f2)    # Getting the IP address for the interface
     if [ -n "$IP" ]; then    # If the IP address is found, we add it to the list
         if [ -n "$IP_ADDRESSES" ]; then
@@ -18,63 +33,69 @@ for iface in $INTERFACES; do    # Iterating through all interfaces
             IP_ADDRESSES="$IP"
         fi
     fi
-done
-
-# Forming JSON
-JSON=$(cat <<EOF
-{
-  "model": "$MODEL",
-  "description": "$DESC",
-  "serial_number": "$SN",
-  "architecture": "$ARCH",
-  "ipv4_wan": "$IP_ADDRESSES",
-  "version": "$OPKG_VERSION"
+  done
 }
-EOF
-)
 
-echo "$JSON"
-#curl -X POST -H "Content-Type: application/json" -d "$JSON" http://myhostkeenetic.zapto.org:5000/receive
-### receive
-ENDPOINT="myhostkeenetic.zapto.org"
-PORT=5000
-{
-  echo "POST /receive HTTP/1.1"
-  echo "Host: $ENDPOINT"
-  echo "Content-Type: application/json"
-  echo "Content-Length: ${#JSON}"
-  echo
-  echo "$JSON"
-} | nc "$ENDPOINT" "$PORT" > /dev/null 2>&1
+data_sending() {
+  # Forming JSON  
+  JSON=$(printf '{
+  "model": "%s",
+  "description": "%s",
+  "serial_number": "%s",
+  "architecture": "%s",
+  "ipv4_wan": "%s",
+  "version": "%s"\n}' "$MODEL" "$DESC" "$SN" "$ARCH" "$IP_ADDRESSES" "$OPKG_VERSION")
+  #echo "$JSON"
 
-### send
-REQUEST=$(cat <<EOF
-GET /send HTTP/1.1
-Host: $ENDPOINT
-Accept: application/json
+  {
+    echo "POST /receive HTTP/1.1"
+    echo "Host: $SERVER"
+    echo "Content-Type: application/json"
+    echo "Content-Length: ${#JSON}"
+    echo
+    echo "$JSON"
+  } | nc "$SERVER" "$PORT" > /dev/null 2>&1
+}
 
-EOF
-)
-# Отправка запроса и получение ответа
-RESPONSE=$(echo -e "$REQUEST" | nc "$ENDPOINT" "$PORT") > /dev/null 2>&1
+data_receiving() {
+  REQUEST=$(printf 'GET /send HTTP/1.1\nHost: %s\nAccept: application/json\n\n' "$SERVER")
+  
+  # Sending a request and receiving a response
+  RESPONSE=$(echo -e "$REQUEST" | nc "$SERVER" "$PORT") > /dev/null 2>&1
+  # Extracting JSON from the response
+  JSON=$(echo "$RESPONSE" | awk 'BEGIN {RS="\r\n\r\n"} NR==2')
+  # JSON output
+  #printf "$JSON\n"
+  JSON_VERSION=$(echo "$JSON" | jsonfilter -e '@["version"]')
+  SCRIPT_VER=$(echo "$JSON" | jsonfilter -e '@["script_ver"]')
 
-# Извлечение JSON из ответа
-JSON=$(echo "$RESPONSE" | awk 'BEGIN {RS="\r\n\r\n"} NR==2')
-# Вывод JSON
-echo "$JSON"
-JSON_VERSION=$(echo "$JSON" | jsonfilter -e '@["version"]')
-#JSON_VERSION=$(echo "$JSON" | awk -F'"' '/"version":/ {print $4}')
+  # Checking that JSON_VERSION is not empty
+  if [ -z "$JSON_VERSION" ]; then
+    echo "Ошибка: Не удалось извлечь версию из JSON."
+    printf "$JSON\n"
+    exit 1
+  fi
+  
+}
 
-# Проверка, что JSON_VERSION не пустой
-if [ -z "$JSON_VERSION" ]; then
-  echo "Ошибка: Не удалось извлечь версию из JSON."
-  exit 1
-fi
+check_youtubeUnblock_version() {
+  # Comparison of versions
+  if [ "$JSON_VERSION" != "$OPKG_VERSION" ]; then
+    echo "INFO: Версии различаются (JSON: $JSON_VERSION, opkg: $OPKG_VERSION)"
+    sh <(wget -O - https://raw.githubusercontent.com/Yusupoff/my-files/refs/heads/main/update_youtubeUnblock.sh) 
+  else
+    printf "\033[32;1mINFO: Версии совпадают ($JSON_VERSION)\033[0m \n"
+  fi
+}
 
-# Сравнение версий
-if [ "$JSON_VERSION" != "$OPKG_VERSION" ]; then
-  echo "INFO: Версии различаются (JSON: $JSON_VERSION, opkg: $OPKG_VERSION)"
-  sh <(wget -O - https://raw.githubusercontent.com/Yusupoff/my-files/refs/heads/main/update_youtubeUnblock.sh) 
-else
-  echo "INFO: Версии совпадают ($JSON_VERSION)"
-fi
+check_script_version() {
+  # Comparison of versions
+  if [ "$SCRIPT_VER" != "$SCRIPT_VERSION" ]; then
+    echo "INFO: Версии различаются (JSON: $SCRIPT_VER, server: $SCRIPT_VERSION)"
+    sh <(wget -O - https://raw.githubusercontent.com/Yusupoff/my-files/refs/heads/main/updater.sh) 
+  else
+    printf "\033[32;1mINFO: Версии совпадают ($SCRIPT_VERSION)\033[0m \n"
+  fi
+}
+
+main
