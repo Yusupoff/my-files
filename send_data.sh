aa9dcc4101860e840b87bf104c2a0384cb5f1b1d
Yusupoff <baitmail@ya.ru>
2025-04-04 04:37:45 +0300

    Update send_data.sh

--- send_data.sh
+++ send_data.sh
@@ -6,7 +6,6 @@ PORT=5000
 MODEL=$(ubus call system board | jsonfilter -e '@["model"]')
 DESC=$(ubus call system board | jsonfilter -e '@["release"]["description"]')
 SN=$([ -z "$(fw_printenv SN | grep 'SN=' | awk -F'=' '{print $2}')" ] && ifconfig br-lan | awk '/HWaddr/ {print $5}')
-MAC_ADDR=$(ifconfig br-lan | awk '/HWaddr/ {print $5}')
 ARCH=$(opkg info kernel  | grep 'Architecture:' | awk '{print $2}')
 IPV4_WAN=$(ubus call network.interface.wan status | jsonfilter -e '@["ipv4-address"][0]["address"]')
 OPKG_VERSION=$(opkg info youtubeUnblock | grep 'Version:' | awk '{print $2}' | cut -d'~' -f1)
@@ -42,10 +41,10 @@ data_sending() {
   JSON=$(printf '{
   "model": "%s",
   "description": "%s",
-  "serial_number": "%s | %s",
+  "serial_number": "%s",
   "architecture": "%s",
   "ipv4_wan": "%s",
-  "version": "%s"\n}' "$MODEL" "$DESC" "$SN" "$MAC_ADDR" "$ARCH" "$IP_ADDRESSES" "$OPKG_VERSION")
+  "version": "%s"\n}' "$MODEL" "$DESC" "$SN" "$ARCH" "$IP_ADDRESSES" "$OPKG_VERSION")
   #echo "$JSON"
 
   {
