45ab2ad6251c3ff2d2661c2369e5dad6e8396369
Yusupoff <baitmail@ya.ru>
2025-05-28 01:54:16 +0300

    Update send_data2.sh

--- send_data2.sh
+++ send_data2.sh
@@ -66,7 +66,7 @@ check_app_version() {
     fi
     printf "\033[33;1mINFO: Версии zapret различаются (JSON: $JSON_VERSION, opkg: $OPKG_VERSION)\033[0m \n"
     wget -q -O /tmp/update_apps.sh https://raw.githubusercontent.com/Yusupoff/my-files/refs/heads/main/update_apps.sh > /dev/null 2>&1
-	/tmp/update_apps.sh
+    /tmp/update_apps.sh
   else
     printf "\033[32;1mINFO: Версии zapret совпадают ($JSON_VERSION)\033[0m \n"
   fi
@@ -94,7 +94,6 @@ main() {
   data_receiving
   check_app_version
   check_script_version
-
 }
 
 main
