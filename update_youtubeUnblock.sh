ae42824c76947787d5e15bd344451fd2d61cd913
Yusupoff <baitmail@ya.ru>
2025-08-28 19:02:56 +0300

    update_youtubeUnblock.sh обновление

--- update_youtubeUnblock.sh
+++ update_youtubeUnblock.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-SCRIPT_VERSION="2.1"
+SCRIPT_VERSION="2.2"
 
 msg_i() { printf "\033[32;1m%s\033[0m\n" "$1"; }
 msg_e() { printf "\033[31;1m%s\033[0m\n" "$1"; }
@@ -64,10 +64,10 @@ data_receiving() {
 
 download_install() {
   msg_i "Загрузка пакетов:"
-  wget "https://github.com/Waujito/youtubeUnblock/releases/download/v1.0.0/youtubeUnblock-${VERSION}-${ARCH}-openwrt-23.05.ipk" -O "/tmp/youtubeUnblock_${ARCH}.ipk" >/dev/null 2>/dev/null && msg_i " youtubeUnblock_${VERSION}_${ARCH}.ipk загружен" || {
+  wget "https://github.com/Waujito/youtubeUnblock/releases/download/v1.1.0/youtubeUnblock-${VERSION}-${ARCH}-openwrt-23.05.ipk" -O "/tmp/youtubeUnblock_${ARCH}.ipk" >/dev/null 2>/dev/null && msg_i " youtubeUnblock_${VERSION}_${ARCH}.ipk загружен" || {
     msg_e " Ошибка скачивания youtubeUnblock_${VERSION}_${ARCH}.ipk"
   }
-  wget "https://github.com/Waujito/youtubeUnblock/releases/download/v1.0.0/luci-app-youtubeUnblock-${VERSION}.ipk" -O "/tmp/luci-app-youtubeUnblock.ipk" 2>/dev/null && msg_i " luci-app-youtubeUnblock_${VERSION}.ipk загружен" || {
+  wget "https://github.com/Waujito/youtubeUnblock/releases/download/v1.1.0/luci-app-youtubeUnblock-${VERSION}.ipk" -O "/tmp/luci-app-youtubeUnblock.ipk" 2>/dev/null && msg_i " luci-app-youtubeUnblock_${VERSION}.ipk загружен" || {
     msg_e " Ошибка скачивания luci-app-youtubeUnblock_${VERSION}.ipk"
   }
   msg_i "Устоновка пакетов"
@@ -78,20 +78,42 @@ download_install() {
 
 config_youtubeUnblock() {
   msg_i "Настройка youtubeUnblock"
-  uci del youtubeUnblock.cfg02d2da.sni_domains
-  uci add_list youtubeUnblock.cfg02d2da.sni_domains='googlevideo.com'
-  uci add_list youtubeUnblock.cfg02d2da.sni_domains='ggpht.com'
-  uci add_list youtubeUnblock.cfg02d2da.sni_domains='ytimg.com'
-  uci add_list youtubeUnblock.cfg02d2da.sni_domains='youtube.com'
-  uci add_list youtubeUnblock.cfg02d2da.sni_domains='play.google.com'
-  uci add_list youtubeUnblock.cfg02d2da.sni_domains='youtu.be'
-  uci add_list youtubeUnblock.cfg02d2da.sni_domains='googleapis.com'
-  uci add_list youtubeUnblock.cfg02d2da.sni_domains='googleusercontent.com'
-  uci add_list youtubeUnblock.cfg02d2da.sni_domains='gstatic.com'
-  uci add_list youtubeUnblock.cfg02d2da.sni_domains='l.google.com'
-  uci add_list youtubeUnblock.cfg02d2da.sni_domains='1e100.net'
-  uci set youtubeUnblock.youtubeUnblock.post_args='--silent'
-  uci commit
+  while uci -q delete youtubeUnblock.@section[0]; do :; done
+  uci set youtubeUnblock.youtubeUnblock.conf_strat='ui_flags'
+  uci set youtubeUnblock.youtubeUnblock.packet_mark='32768'
+  uci set youtubeUnblock.youtubeUnblock.queue_num='537'
+  uci set youtubeUnblock.youtubeUnblock.silent='0'
+  uci set youtubeUnblock.youtubeUnblock.no_ipv6='1'
+  uci add youtubeUnblock section # =cfg02d2da
+  uci set youtubeUnblock.@section[-1].name='youtube'
+  uci set youtubeUnblock.@section[-1].enabled='1'
+  uci set youtubeUnblock.@section[-1].tls_enabled='1'
+  uci set youtubeUnblock.@section[-1].fake_sni='1'
+  uci set youtubeUnblock.@section[-1].faking_strategy='pastseq'
+  uci set youtubeUnblock.@section[-1].fake_sni_seq_len='1'
+  uci set youtubeUnblock.@section[-1].fake_sni_type='default'
+  uci set youtubeUnblock.@section[-1].frag='tcp'
+  uci set youtubeUnblock.@section[-1].frag_sni_reverse='1'
+  uci set youtubeUnblock.@section[-1].frag_sni_faked='0'
+  uci set youtubeUnblock.@section[-1].frag_middle_sni='1'
+  uci set youtubeUnblock.@section[-1].frag_sni_pos='1'
+  uci set youtubeUnblock.@section[-1].seg2delay='0'
+  uci set youtubeUnblock.@section[-1].fk_winsize='0'
+  uci set youtubeUnblock.@section[-1].synfake='0'
+  uci set youtubeUnblock.@section[-1].sni_detection='parse'
+  uci set youtubeUnblock.@section[-1].all_domains='0'
+  uci add_list youtubeUnblock.@section[-1].sni_domains='googlevideo.com'
+  uci add_list youtubeUnblock.@section[-1].sni_domains='ggpht.com'
+  uci add_list youtubeUnblock.@section[-1].sni_domains='ytimg.com'
+  uci add_list youtubeUnblock.@section[-1].sni_domains='youtube.com'
+  uci add_list youtubeUnblock.@section[-1].sni_domains='play.google.com'
+  uci add_list youtubeUnblock.@section[-1].sni_domains='youtu.be'
+  uci add_list youtubeUnblock.@section[-1].sni_domains='googleapis.com'
+  uci add_list youtubeUnblock.@section[-1].sni_domains='googleusercontent.com'
+  uci add_list youtubeUnblock.@section[-1].sni_domains='gstatic.com'
+  uci add_list youtubeUnblock.@section[-1].sni_domains='l.google.com'
+  uci set youtubeUnblock.@section[-1].quic_drop='1'
+  uci commit youtubeUnblock
   /etc/init.d/youtubeUnblock restart >/dev/null 2>&1
 }
 
